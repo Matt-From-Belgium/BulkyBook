c1d77a995f470aed479f6ab2ec614589591833ad
Matthias Bauw <matthias.bauw@gmail.com>
2021-11-29 09:29:41 +0100

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
