1d642a8bd625d407a9f1b608e55b39d72dc4284f
Matthias Bauw <matthias.bauw@gmail.com>
2021-12-02 19:59:39 +0100

    Implementatie basisklassen repository pattern

---
+++ BulkyBook.DataAccess/Repository/IRepostory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public interface IRepostory<T> where T : class
+    {
+        IEnumerable<T> getAll();
+        T getFirstOrDefault(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
