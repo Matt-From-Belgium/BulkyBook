fc2e970d6f0fec6a91b4d1bba4c3f246626453bd
Matthias Bauw <matthias.bauw@gmail.com>
2021-12-03 17:29:55 +0100

    Cover type display name toegevoegd.

--- BulkyBook.Models/CoverType.cs
+++ BulkyBook.Models/CoverType.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name ="Cover type name")]
         public string Name { get; set; }
     }
 }
