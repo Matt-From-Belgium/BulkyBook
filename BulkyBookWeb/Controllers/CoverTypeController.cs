6b1a4ca871457b3f6fdb330bbb8f5ba5e975a25c
Matthias Bauw <matthias.bauw@gmail.com>
2021-12-03 11:39:31 +0100

    Cover type list geïmplementeerd

---
+++ BulkyBookWeb/Controllers/CoverTypeController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var ctList = _unitOfWork.CoverType.getAll();
+            return View(ctList);
+        }
+    }
+}
